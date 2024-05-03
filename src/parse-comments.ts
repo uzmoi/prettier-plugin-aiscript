1f004ab5d0cfce23118de9149eac594716f23e02
uzmoi <rizzzse@gmail.com>
2024-05-04 08:10:38 +0900

    add comments parser

---
+++ src/parse-comments.ts
@@ -0,0 +1,47 @@
+import { Ast } from "@syuilo/aiscript";
+import { parse } from "@syuilo/aiscript/parser/parser.js";
+
+// コメントの構文
+// https://github.com/aiscript-dev/aiscript/blob/master/src/parser/parser.peggy#L30
+
+const getCommentLocations = (source: string, preprocessedString: string) => {
+    const comments: Ast.Loc[] = [];
+
+    let state: null | { type: "slc" | "mlc"; start: number } = null;
+
+    for (let i = 0, j = 0; i < source.length; i++) {
+        if (state === null) {
+            if (source[i] === preprocessedString[j++]) continue;
+
+            switch (source.slice(i, i + 2)) {
+                case "//":
+                    state = { type: "slc", start: i };
+                    break;
+                case "/*":
+                    state = { type: "mlc", start: i };
+                    break;
+                default:
+                    throw new Error(
+                        "Preprocessにコメント以外の差分が存在します。",
+                    );
+            }
+        } else if (
+            (state.type === "slc" && source[i] === "\n") ||
+            (state.type === "mlc" && source.slice(i - 2, i) === "*/")
+        ) {
+            comments.push({ start: state.start, end: i });
+            state = null;
+        }
+    }
+
+    return comments;
+};
+
+export const parseComments = (source: string): [Ast.Loc, string][] => {
+    const preprocessedString = parse(source, { startRule: "Preprocess" });
+
+    return getCommentLocations(source, preprocessedString).map(range => [
+        range,
+        source.slice(range.start, range.end),
+    ]);
+};
