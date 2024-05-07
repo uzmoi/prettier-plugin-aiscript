903f75f9bc633827dd2cff62c583b5f29df4c704
uzmoi <rizzzse@gmail.com>
2024-05-07 09:55:34 +0900

    init website

---
+++ website/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+    <h1>prettier-plugin-aiscript playground</h1>
+</main>
+
+<style>
+    :global(*, ::before, ::after) {
+        box-sizing: border-box;
+        margin: 0;
+    }
+</style>
