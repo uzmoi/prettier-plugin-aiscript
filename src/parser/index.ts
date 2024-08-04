b46eb9979d35c692f3cc3d8639d8da1e54a147b0
uzmoi <rizzzse@gmail.com>
2024-08-04 14:39:23 +0900

    0.19.0に対応

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,24 +1,64 @@
-import { Parser } from "@syuilo/aiscript";
+import { AISCRIPT_VERSION, type Ast, Parser } from "@syuilo/aiscript";
 import type { Root } from "../node";
 import {
 	correctLocation,
 	parseCommentsByPreprocessDiff,
+	parseCommentsByStringLocations,
 } from "./parse-comments";
 import { transformChainPlugin } from "./transform-chain";
 import { parserPlugin } from "./utils";
 
-export const parse = (text: string): Root => {
-	const comments = parseCommentsByPreprocessDiff(text);
+const parse_0_19_0 = (source: string): Root => {
+	const parser = new Parser();
+	parser.addPlugin("transform", transformChainPlugin());
+	parser.addPlugin(
+		"transform",
+		parserPlugin(node => {
+			if (node.loc == null) return node;
+			const { start, end } = node.loc;
+			// AiScriptのパーサーがendに最後の文字のindexを入れるので+1する。
+			return { ...node, loc: { start, end: end + 1 } };
+		}),
+	);
+
+	const stringLocations: Ast.Loc[] = [];
+	parser.addPlugin(
+		"transform",
+		// collect string locations
+		parserPlugin(node => {
+			const isStringNode = node.type === "str" || node.type === "tmpl";
+			if (isStringNode && node.loc) {
+				stringLocations.push(node.loc);
+			}
+			return node;
+		}),
+	);
+
+	const body = parser.parse(source);
+
+	const comments = parseCommentsByStringLocations(source, stringLocations);
+
+	return { type: "root", body, comments };
+};
+
+const parse_before_0_19_0 = (source: string): Root => {
+	const comments = parseCommentsByPreprocessDiff(source);
 
 	const parser = new Parser();
-
 	parser.addPlugin("transform", transformChainPlugin());
 	parser.addPlugin(
 		"transform",
 		parserPlugin(node => correctLocation(node, comments)),
 	);
 
-	const body = parser.parse(text);
+	const body = parser.parse(source);
 
 	return { type: "root", body, comments };
+};
+
+export const parse = (text: string): Root => {
+	if ((AISCRIPT_VERSION as string) === "0.19.0") {
+		return parse_0_19_0(text);
+	}
+	return parse_before_0_19_0(text);
 };
