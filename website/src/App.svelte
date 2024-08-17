1fa71de52c006d52cfe0ad5b7e8a237bae3ba6a4
uzmoi <rizzzse@gmail.com>
2024-08-17 21:31:07 +0900

    websiteにnpmリンクを追加

--- website/src/App.svelte
+++ website/src/App.svelte
@@ -2,6 +2,11 @@
 	import { AISCRIPT_VERSION } from "@syuilo/aiscript/constants.js";
 	import Playground from "./Playground.svelte";
 	import initialCode from "./assets/playground-initial-code.ais?raw";
+
+	const links: [string, string][] = [
+		["NPM", "https://www.npmjs.com/package/@uzmoi/prettier-plugin-aiscript"],
+		["GitHub", "https://github.com/uzmoi/prettier-plugin-aiscript"],
+	];
 </script>
 
 <div id="root">
@@ -10,12 +15,13 @@
 			<h1>prettier-plugin-aiscript playground</h1>
 			<p>AiScript {AISCRIPT_VERSION}</p>
 		</div>
-		<a
-			href="https://github.com/uzmoi/prettier-plugin-aiscript"
-			rel="noreferrer"
-		>
-			GitHub
-		</a>
+		<ul>
+			{#each links as [name, href]}
+				<li>
+					<a {href} rel="noreferrer">{name}</a>
+				</li>
+			{/each}
+		</ul>
 	</header>
 	<main>
 		<Playground value={initialCode} />
@@ -43,6 +49,13 @@
 	h1 {
 		font-size: x-large;
 		line-height: 1.2;
+	}
+
+	ul {
+		list-style: none;
+		padding: 0;
+		display: inline-flex;
+		gap: 0.75em;
 	}
 
 	a {
