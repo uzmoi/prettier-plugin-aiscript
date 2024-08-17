9619f64b77dc4679416ca8e354cbd0f46e4661a3
uzmoi <rizzzse@gmail.com>
2024-08-18 03:24:58 +0900

    カバレッジをciでのみ計るようにする

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { coverageConfigDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		coverage: {
-			enabled: true,
 			exclude: [...coverageConfigDefaults.exclude, "website/**"],
 		},
 	},
